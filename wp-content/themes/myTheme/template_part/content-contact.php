d146acb1a48d75022a629a01ea05cb4f83f822ca
SangDang <quangsang222@gmail.com>
2019-08-23 00:35:31 +0700

    a

--- wp-content/themes/myTheme/template_part/content-contact.php
+++ wp-content/themes/myTheme/template_part/content-contact.php
@@ -7,11 +7,11 @@ $slideBanner = get_field('slide_banner', 'option');
             <span class="text-uppercase"><?php echo $slideBanner['title'] ?></span>
         </div>
     </div>
-    <div class="fixed-width wow fadeInDown" style="padding: 0 15px">
-        <div style="max-width: 1500px" class="m-auto">
+    <div class="wow fadeInDown" style="padding: 0 15px">
+        <div class="m-auto">
             <div class="owl-carousel owl-theme " id="contact-carousel">
                 <?php foreach ($slideBanner['list_image'] as $item_contact): ?>
-                    <div class="" style="position: relative; width: auto; height: 405px; background: url(<?php echo $item_contact['image']['url'] ?>) center center no-repeat; background-size: cover">
+                    <div class="" style="position: relative; width: auto; height: 555px; background: url(<?php echo $item_contact['image']['url'] ?>) center center no-repeat; background-size: cover">
                         <a href="<?php echo $item_contact['link']['url'] ?>"
                            target="_blank"
                            style="position: absolute; left: 0; top: 0;width: 100%; height: 100%;"
