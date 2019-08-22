8d45800fce64e92589ce4b5730bdc1bea8ef3a17
Your Name <hoang.cao@alta.com.vn>
2019-08-22 23:43:16 +0700

    share

--- wp-content/themes/myTheme/js/myJs/index.js
+++ wp-content/themes/myTheme/js/myJs/index.js
@@ -105,15 +105,9 @@ window.addEventListener("load", function (event) {
 $(document).ready(function () {
     //funciton
     // When the user clicks anywhere outside of the modal, close it
-    $("#ship-to-different-address-checkbox").attr('checked', false)
-    $("#check-difer").click(function (e) {
-        $("#ship-to-different-address-checkbox").trigger('click');
-        if (e.target.checked) {
-            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeIn('slow')
-        } else {
-            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeOut('slow')
-        }
-    })
+    //shipping
+
+    //zalo share
     window.onclick = function (event) {
         if ($(event.target).hasClass('close-custom')) {
             $('.zoomContainer').remove();
--- wp-content/themes/myTheme/js/myJs/single-product.js
+++ wp-content/themes/myTheme/js/myJs/single-product.js
@@ -1,5 +1,19 @@
 var language_current = $('#language_current').attr('lang')
 $(document).ready(function () {
+    $("#ship-to-different-address-checkbox").attr('checked', false)
+    $("#check-difer").click(function (e) {
+        $("#ship-to-different-address-checkbox").trigger('click');
+        if (e.target.checked) {
+            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeIn('slow')
+        } else {
+            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeOut('slow')
+        }
+    })
+    //zalo share
+    $(".zalo-share").click(function (e) {
+        e.preventDefault();
+        $(".zalo-share-button").trigger('click')
+    })
     settingSlider();
     viewVariable();
     submitProductVariable();
