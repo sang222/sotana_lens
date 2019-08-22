8d05a6b3776e3162be0f353e6ec7abf6f0448043
Your Name <hoang.cao@alta.com.vn>
2019-08-23 00:49:56 +0700

    :D

--- wp-content/themes/myTheme/js/myJs/checkout.js
+++ wp-content/themes/myTheme/js/myJs/checkout.js
@@ -1,25 +1,12 @@
 $(document).ready(function () {
-    // $("#checkout_now").click(function () {
-    //     $("#place_order").trigger('click');
-    // })
+    $("#ship-to-different-address-checkbox").attr('checked', false)
+    $("#check-difer").click(function (e) {
 
-    // if ($("input[value='free_shipping:1']").val() == 'free_shipping:1' && $("input[value='free_shipping:1']").attr('checked') == 'checked') {
-    //     $(".account-back").css({'display': 'block'})
-    // }
-    // $("input[name='terms']").attr('checked', true)
+        $("#ship-to-different-address-checkbox").trigger('click');
+        if (e.target.checked) {
+            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeIn('slow')
+        } else {
+            $('.shipping_address .woocommerce-shipping-fields__field-wrapper').fadeOut('slow')
+        }
+    })
 })
-$(document).on('click', '.shipping_method', function (e) {
-    // alert(1) free_shipping:1
-    // if (e.target.value == 'free_shipping:1') {
-    //     $(".account-back").css({'display': 'block'})
-    //     scrollToSection('.account-back')
-    // } else {
-    //     $(".account-back").css({'display': 'none'})
-    // }
-})
-
-function scrollToSection(target) {
-    // $('html,body').stop().animate({
-    //     scrollTop: $(target).offset().top - $(".header-nav").outerHeight() - 20
-    // }, 1000);
-}
