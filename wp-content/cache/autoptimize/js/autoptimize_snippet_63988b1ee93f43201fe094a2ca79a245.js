08dc7330d42db43f0302545225b50c50b50b3a93
SangDang <quangsang222@gmail.com>
2019-09-24 22:38:22 +0700

    delete cache

--- wp-content/cache/autoptimize/js/autoptimize_snippet_63988b1ee93f43201fe094a2ca79a245.js
+++
@@ -1,3 +0,0 @@
-$(document).ready(function(){toggleFilter();})
-function toggleFilter(){$(".filter-list h5").click(function(){$(this).next().toggleClass('d-none')
-$(this).find('span i').toggleClass('fa-angle-right').toggleClass('fa-angle-down')})};
