3e0a13b853047742d5e43e4965a961b8e859fd3d
SangDang <quangsang222@gmail.com>
2019-09-28 10:18:48 +0700

    fix responsive menu

--- wp-content/themes/myTheme/header.php
+++ wp-content/themes/myTheme/header.php
@@ -159,7 +159,7 @@
                         ));
                     ?>
                 </div>
-                <div style="display: flex; align-items: center" class="d-none">
+                <div style="display: flex; align-items: center">
                     <div class="container-mini-cart"></div>
                     <div>
                         <div class="menu-toggle">
